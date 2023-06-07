7b0740ae0183fae800da76394837f923523bb152
1101229664 <46476290+1101229664@users.noreply.github.com>
2023-06-08 00:37:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-city_id = os.environ['CITYID']
+city_id = os.environ['CITY_ID']
 birthday = os.environ['BIRTHDAY']
 
 app_id = os.environ["APP_ID"]
